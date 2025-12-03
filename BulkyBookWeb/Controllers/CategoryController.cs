e2c164e225f459c47e55c812d40b20b7e347c83f
Sonu Yagyasaini <SonuYagyasaini@outlook.com>
2025-12-03 18:55:37 +0530

    toaster notification working now

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,21 +1,22 @@
 ﻿using Bulky.Models;
 using Bulky.DataAccess.Data;
-using Microsoft.AspNetCore.Mvc; 
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Mvc;
 using Bulky.DataAccess.Repository.IRepository;
 
 namespace BulkyBookWeb.Controllers
-{ 
+{
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            var objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -28,31 +29,35 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            if(category.Name == category.DisplayOrder.ToString())
+            if (category.Name == category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("DisplayOrder", "The DisplayOrder cann't exactly match the name.");
+                ModelState.AddModelError(nameof(Category.DisplayOrder), "The DisplayOrder cannot exactly match the Name.");
             }
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
                 _unitOfWork.Category.Add(category);
                 _unitOfWork.Save();
                 TempData["Success"] = "Category created successfully.";
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
-            return View(category );
+
+            return View(category);
         }
 
         public IActionResult Edit(int? id)
         {
-            if (id == null || id==0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            var category = _unitOfWork.Category.Get(x=> x.Id==id);
+
+            var category = _unitOfWork.Category.Get(x => x.Id == id);
             if (category == null)
             {
                 return NotFound();
             }
+
             return View(category);
         }
 
@@ -62,15 +67,17 @@ namespace BulkyBookWeb.Controllers
         {
             if (category.Name == category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("DisplayOrder", "The DisplayOrder cann't exactly match the name.");
+                ModelState.AddModelError(nameof(Category.DisplayOrder), "The DisplayOrder cannot exactly match the Name.");
             }
+
             if (ModelState.IsValid)
             {
                 _unitOfWork.Category.Update(category);
                 _unitOfWork.Save();
                 TempData["Success"] = "Category updated successfully.";
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
+
             return View(category);
         }
 
@@ -80,27 +87,35 @@ namespace BulkyBookWeb.Controllers
             {
                 return NotFound();
             }
+
             var category = _unitOfWork.Category.Get(x => x.Id == id);
             if (category == null)
             {
                 return NotFound();
             }
+
             return View(category);
         }
 
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteById(int? id)
-        { 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
             var getCategory = _unitOfWork.Category.Get(x => x.Id == id);
             if (getCategory == null)
             {
                 return NotFound();
             }
+
             _unitOfWork.Category.Remove(getCategory);
             _unitOfWork.Save();
             TempData["Success"] = "Category deleted successfully.";
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
